e254159059d59b5d4fc271ad16abe623c3b21fa5
unknown <Estacio@RRBVTESL04004.estacio.acad>
2017-10-23 21:56:15 -0400

    novo

---
+++ SQL/Novas Tabelas.sql
@@ -0,0 +1,38 @@
+﻿CREATE TABLE tipo(
+	id serial, 
+	nome varchar(100),
+
+	CONSTRAINT pk_tipo PRIMARY KEY(id)
+);
+
+CREATE TABLE usuario(
+	id serial, 
+	nome varchar(100),
+	sobrenome varchar(100),
+	email varchar(100),
+	senha varchar(100),
+	tipo integer,
+
+	CONSTRAINT pk_usuario PRIMARY KEY(id),
+	CONSTRAINT fk_tipo_usuario FOREIGN KEY(tipo)
+		REFERENCES tipo(id)
+);
+
+CREATE TABLE categoria(
+	id serial, 
+	nome varchar(100),
+
+	CONSTRAINT pk_categoria PRIMARY KEY(id)
+);
+
+CREATE TABLE produto(
+	id serial, 
+	nome varchar(100),
+	descricao varchar(100),
+	categoria integer,
+	preco float,
+
+	CONSTRAINT pk_produto PRIMARY KEY(id),
+	CONSTRAINT fk_categoria_produto FOREIGN KEY(categoria)
+		REFERENCES categoria(id)
+);
---
+++ SQL/Novos Dados.sql
@@ -0,0 +1,27 @@
+﻿INSERT INTO categoria
+	(nome)
+	VALUES('Informática');
+	
+INSERT INTO categoria
+	(nome)
+	VALUES('Papelaria');
+
+INSERT INTO tipo
+	(nome)
+	VALUES('Administrador');
+	
+INSERT INTO tipo
+	(nome)
+	VALUES('Usuario');
+
+INSERT INTO produto
+	(nome, descricao, categoria, preco)
+	VALUES('Notebook', 'Notebook game i7 Inspirion', 1, 6320.42);
+
+INSERT INTO produto
+	(nome, descricao, categoria, preco)
+	VALUES('Agenda Pequena', '200 flohas', 2, 25.50);
+
+INSERT INTO usuario
+	(nome, sobrenome, email, senha, tipo)
+	VALUES('admin', 'root', 'admin@root', '123', 1);
